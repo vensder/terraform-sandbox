d6d39af1d954e9367c5dae6f4174bb997e73e747
Dmitrii Makarov <vensder@gmail.com>
2020-07-13 15:06:56 +1200

    moved vpc to network template

--- network.tf
+++ network.tf
@@ -1,3 +1,14 @@
+# Create VPC
+module "vpc" {
+  source        = "./modules/vpc"
+  vpc_cidr      = "10.0.2.0/23"
+  subnet_a_cidr = "10.0.2.0/24"
+  subnet_b_cidr = "10.0.3.0/24"
+  vpc_id        = "${module.vpc.vpc_id}"
+  aws_region    = "${var.aws_region}"
+  name_tag      = "my-sandbox"
+}
+
 # Create Internet Gateway
 resource "aws_internet_gateway" "sandbox-igw" {
   vpc_id = "${module.vpc.vpc_id}"
