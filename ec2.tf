79ebc432bcb8611cd37b1f732ddf4a24a4721639
Dmitrii Makarov <vensder@gmail.com>
2020-07-16 19:54:23 +1200

    added ssh pub key

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,14 @@
+resource "aws_key_pair" "my_key" {
+  key_name   = "my_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfKSiUnU/u8WSn1VWab7p4xJuZgOnN9ydbKUGAGGU5jCSMH2njWsU4KRtdvrWGcSkFSNiufzFs6cXqIOi1fhM6t5weJGLsqL9cpRQgS7oUSMOeo6vii45xksNUU4tLHjz6RCCOnKmleUwPmFffVrWiCgnzwQcWBQQ7wvLoUt2qC58b9dFuh2ueNizanWaCTXCL2675on6kvB9rfbQXJ1cZy+y/z0Tsxp74wv3AuzclZDUSoX5xcEwJr7VN6HPfjAm0ZpJxHPgjPXoceqzWVs896pEfOqxAKH0eK4FW5FBM8EQSjspjpehYM4pWT4h5VmhEznYcD0kBaAopvdxSwg4P my@key"
+}
+
 resource "aws_instance" "nginx" {
-
-  ami           = "${lookup(var.ami, var.aws_region)}"
-  instance_type = "t3.nano"
-
-  # VPC
-  subnet_id = "${module.vpc.subnet_a_id}"
-
-  # Security Group
+  key_name               = "${aws_key_pair.my_key.key_name}"
+  ami                    = "${lookup(var.ami, var.aws_region)}"
+  instance_type          = "t3.nano"
+  subnet_id              = "${module.vpc.subnet_a_id}"
   vpc_security_group_ids = ["${aws_security_group.ssh_http_allowed.id}"]
-
 
   user_data = <<EOF
 #!/usr/bin/env bash
@@ -24,6 +24,6 @@ echo '''
 EOF
 
   tags = {
-    Name = "MyAwesomeInstance"
+    Name = "my_instance"
   }
 }
